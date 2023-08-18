83897b470b872fd69242152fb073a3925d96673e
Eliana Jones <133198355+elainajones@users.noreply.github.com>
2023-08-17 21:03:23 -0700

    feat: Add python solution for problem 10

---
+++ elaina/python/problem-10.py
@@ -0,0 +1,23 @@
+# PROBLEM 10
+#
+# The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
+#
+# Find the sum of all the primes below two million.
+#
+# Duration: 22.68 minutes
+# Duration on server: 13.21 minutes
+primes = [2,3]
+test_set = []
+x=0
+for t in range(2,2000001):
+    for p in primes:
+        if t % p == 0:
+            break
+        elif p > int(t/primes[x]):
+            break
+    if t % p != 0:
+        primes.append(t)
+    elif t >= primes[x]*primes[x+1]:
+        x+=1
+
+print(f"Sum of primes:\t{sum(primes)-3}")
