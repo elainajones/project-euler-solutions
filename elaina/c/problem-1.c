d11376c6d205589fb0d169d488e8d030d9ca812e
Elaina Jones <133198355+elainajones@users.noreply.github.com>
2024-03-29 00:42:26 -0700

    style: Format equal signs and add question
    Adds spaces around the '+=' signs for readability and adds the
    Project Euler question to a comment at the top.

--- elaina/c/problem-1.c
+++ elaina/c/problem-1.c
@@ -1,19 +1,25 @@
+// If we list all the natural numbers below 10 that are multiples of 3 
+// or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+//
+// Find the sum of all the multiples of 3 or 5 below 1000.
 #include <stdio.h>
 
 void main() {
-    int sum = 0;
     int max = 1000;
+    int sum = 0;
+
     int i;
     for (i = 1; i < max; i++) {
         if (i*3 > max && i*5 > max) {
             break;
         }
         if (i*3 < max) {
-            sum+=i*3;
+            sum += i*3;
         }
         if (i*5 < max && (i*5) % 3 != 0) {
-            sum+=i*5;
+            sum += i*5;
         }
     }
-    printf("%d\n",sum);
+
+    printf("%d\n", sum);
 }
